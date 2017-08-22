9337b4fd87159bf50ef0b6fc7349676543b5d4c9
raiden <casaucau.cyril@protonmail.com>
2017-08-22 09:26:29 +0200

    Add account @90% : Validation errors, Deactivation of webcrypto liner

--- src/main/webapp/app/vendor.ts
+++ src/main/webapp/app/vendor.ts
@@ -1,9 +1,5 @@
 /* after changing this file run 'yarn run webpack:build' */
 /* tslint:disable */
 import '../content/scss/vendor.scss';
-import 'webcrypto-core/dist/webcrypto-core.js';
-import 'webcrypto-core/dist/webcrypto-core.es.js';
-import 'webcrypto-liner/dist/webcrypto-liner.lib.js';
-import 'webcrypto-liner/dist/webcrypto-liner.shim.js';
 import 'text-encoding/lib/encoding.js';
 // jhipster-needle-add-element-to-vendor - JHipster will add new menu items here
