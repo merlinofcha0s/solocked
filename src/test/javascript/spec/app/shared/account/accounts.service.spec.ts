c9d9df3c3e2d7d3dd5e9f449306b8a30d74bae18
raiden <casaucau.cyril@protonmail.com>
2017-08-23 09:11:52 +0200

    Add account @100% : Fixing last unit test

--- src/test/javascript/spec/app/shared/account/accounts.service.spec.ts
+++ src/test/javascript/spec/app/shared/account/accounts.service.spec.ts
@@ -1,9 +1,14 @@
+import { SessionStorageService } from 'ng2-webstorage/dist/services';
+import { JhiDataUtils } from 'ng-jhipster';
 import { MockActivatedRoute } from './../../../helpers/mock-route.service';
 import { NinjaccountTestModule } from './../../../test.module';
 import { TestBed, async, tick, fakeAsync, inject } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Rx';
 import { AccountsService } from '../../../../../../main/webapp/app/shared/account/accounts.service';
+import { AccountsDBService } from '../../../../../../main/webapp/app/entities/accounts-db/accounts-db.service';
+import { CryptoUtilsService } from '../../../../../../main/webapp/app/shared/crypto/crypto-utils.service';
+import { CryptoService } from '../../../../../../main/webapp/app/shared/crypto/crypto.service';
 
 describe('Services Tests', () => {
 
@@ -13,7 +18,12 @@ describe('Services Tests', () => {
                 imports: [NinjaccountTestModule],
                 declarations: [],
                 providers: [
-                    AccountsService
+                    AccountsService,
+                    AccountsDBService,
+                    CryptoUtilsService,
+                    JhiDataUtils,
+                    SessionStorageService,
+                    CryptoService
                 ]
             }).compileComponents();
         }));
