34c5ac3d4891931ba1372a815032af0500004308
raiden <casaucau.cyril@protonmail.com>
2017-10-06 09:25:21 +0200

    Details UI @ 90%

--- src/main/webapp/app/connected/accountsdb-details/accountsdb-details.component.ts
+++ src/main/webapp/app/connected/accountsdb-details/accountsdb-details.component.ts
@@ -2,10 +2,11 @@ import {Subscription} from 'rxjs/Rx';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {AccountsService} from './../../shared/account/accounts.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, ElementRef, OnDestroy, OnInit, Renderer, ViewChild} from '@angular/core';
 import {Account} from '../../shared/account/account.model';
 import {MdSnackBar, MdSnackBarConfig} from '@angular/material';
 import {TranslateService} from '@ngx-translate/core';
+import {ClipboardService} from "ngx-clipboard/dist";
 
 @Component({
     selector: 'jhi-accountsdb-details',
@@ -20,10 +21,15 @@ export class AccountsdbDetailsComponent implements OnInit, OnDestroy {
     showPassword: boolean;
     tooltipPosition = 'above';
 
+    @ViewChild('clearClipboardElement')
+    private clearClipboardElement: ElementRef;
+
     constructor(private route: ActivatedRoute, private router: Router,
                 private accountsService: AccountsService,
                 private snackBar: MdSnackBar,
-                private translateService: TranslateService) {
+                private translateService: TranslateService,
+                private clipboard: ClipboardService,
+                private renderer: Renderer) {
     }
 
     ngOnInit() {
@@ -52,12 +58,30 @@ export class AccountsdbDetailsComponent implements OnInit, OnDestroy {
     }
 
     notifyCopyToClipboard(nameField: string) {
-        const config = new MdSnackBarConfig();
-        config.duration = 3000;
-
+        // Translation
         const nameFieldTrans = this.translateService.instant(nameField);
         const textToast = this.translateService.instant('ninjaccountApp.accountsDB.details.toastMessage');
 
+        // Config and show toast message
+        const config = new MdSnackBarConfig();
+        config.duration = 3000;
         this.snackBar.open(nameFieldTrans + ' ' + textToast, '', config);
+
+        //this.startTimeout();
+    }
+
+    startTimeout() {
+        // Observable.timer(1000, 1000)
+        //     .timeInterval()
+        //     .pluck('interval')
+        //     .take(5).subscribe(
+        //     (x) => console.log(x),
+        //     (e) => console.log(e),
+        //     () => {
+        //         this.clearClipboard();
+        //         const config = new MdSnackBarConfig();
+        //         config.duration = 3000;
+        //         this.snackBar.open('Clipboard Cleared !', '', config);
+        //     });
     }
 }
