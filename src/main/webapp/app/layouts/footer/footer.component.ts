eb42b819d06fdf1aab6ab73d9734ba2e52971128
raiden <casaucau.cyril@protonmail.com>
2017-10-25 23:33:40 +0200

    Fix footer problem @50%

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Payment, PlanType} from '../../entities/payment/payment.model';
+import {PaymentService} from '../../entities/payment/payment.service';
+import {Principal} from '../../shared/auth/principal.service';
+import {Subscription} from 'rxjs/Subscription';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
     selector: 'jhi-footer',
-    templateUrl: './footer.component.html'
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit, OnDestroy  {
+
+    payment: Payment;
+    paymentSub: Subscription;
+    displayPaymentIssue: boolean;
+
+    constructor(private paymentService: PaymentService,
+                private principal: Principal,
+                private router: Router) {
+        this.displayPaymentIssue = false;
+    }
+
+    ngOnInit(): void {
+        this.initPaymentService();
+
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                if (event.url === '/') {
+                    this.displayPaymentIssue = false;
+                }
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.paymentSub.unsubscribe();
+    }
+
+    initPaymentService() {
+        this.paymentSub = this.paymentService.payment$.subscribe((payment) => {
+            this.payment = payment;
+            this.displayPaymentIssue = this.isInPaymentWarning(payment);
+        });
+    }
+
+    isInPaymentWarning(payment: Payment): boolean {
+        if (this.principal.isAuthenticated() && !this.principal.hasAnyAuthorityDirect(['ROLE_ADMIN'])
+            && (!payment.paid || payment.planType.toString() === PlanType[PlanType.FREE])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -8,23 +8,16 @@ import {Principal} from '../../shared/auth/principal.service';
 import {Subscription} from 'rxjs/Subscription';
 
 @Component({
-    selector: 'jhi-main',
+    selector: 'body',
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss']
 })
-export class JhiMainComponent implements OnInit, OnDestroy {
+export class JhiMainComponent implements OnInit {
 
     loginPage: boolean;
-    payment: Payment;
-    paymentSub: Subscription;
-    displayPaymentIssue: boolean;
 
     constructor(private jhiLanguageHelper: JhiLanguageHelper,
-                private router: Router,
-                private paymentService: PaymentService,
-                private principal: Principal) {
-        this.displayPaymentIssue = false;
-    }
+                private router: Router) {}
 
     private getPageTitle(routeSnapshot: ActivatedRouteSnapshot) {
         let title: string = (routeSnapshot.data && routeSnapshot.data['pageTitle']) ? routeSnapshot.data['pageTitle'] : 'ninjaccountApp';
@@ -42,33 +35,11 @@ export class JhiMainComponent implements OnInit, OnDestroy {
             if (event instanceof NavigationEnd) {
                 if (event.url === '/') {
                     this.loginPage = true;
-                    this.displayPaymentIssue = false;
                 } else {
                     this.loginPage = false;
                 }
             }
         });
-
-        this.initPaymentService();
     }
 
-    ngOnDestroy(): void {
-        this.paymentSub.unsubscribe();
-    }
-
-    initPaymentService() {
-       this.paymentSub = this.paymentService.payment$.subscribe((payment) => {
-           this.payment = payment;
-           this.displayPaymentIssue = this.isInPaymentWarning(payment);
-       });
-    }
-
-    isInPaymentWarning(payment: Payment): boolean {
-        if (this.principal.isAuthenticated() && !this.principal.hasAnyAuthorityDirect(['ROLE_ADMIN'])
-            && (!payment.paid || payment.planType.toString() === PlanType[PlanType.FREE])) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
