aab726b01f492fa14a64136a32ee43fb78a2deb2
raiden <casaucau.cyril@protonmail.com>
2017-11-03 18:11:05 +0100

    Beginning cleaning up danger zone : CSS + translate for settings page

--- src/main/webapp/app/account/settings/delete-all-accounts/delete-all-accounts.component.ts
+++ src/main/webapp/app/account/settings/delete-all-accounts/delete-all-accounts.component.ts
@@ -1,13 +1,12 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {AccountsService} from '../../../shared/account/accounts.service';
-import {Account} from '../../../shared/account/account.model';
 import {MatDialogRef, MatSnackBar, MatSnackBarConfig} from '@angular/material';
-import {isUndefined} from 'util';
-import {saveAs as importedSaveAs} from 'file-saver';
-import {UserService, LoginService} from '../../../shared/index';
+import {LoginService, UserService} from '../../../shared/index';
 import {Router} from "@angular/router";
 import {TranslateService} from '@ngx-translate/core';
 import {SnackComponent} from "../../../shared/snack/snack.component";
+import {Principal} from "../../../shared/auth/principal.service";
+import {Subscription} from "rxjs/Subscription";
+import {isUndefined} from "util";
 
 @Component({
     selector: 'jhi-delete-all-accounts',
@@ -16,37 +15,54 @@ import {SnackComponent} from "../../../shared/snack/snack.component";
 })
 export class DeleteAllAccountsComponent implements OnInit, OnDestroy {
 
+    private config: MatSnackBarConfig;
+    private deleteAccountSubscription: Subscription;
+
+    username: string;
+    maxUsername = 50;
 
     constructor(private userService: UserService
         , private loginService: LoginService
         , private dialogRef: MatDialogRef<DeleteAllAccountsComponent>
         , private router: Router
         , private snackBar: MatSnackBar
-        ,private translateService: TranslateService,) {
+        , private translateService: TranslateService
+        , private principal: Principal) {
     }
 
     ngOnInit() {
+        this.config = new MatSnackBarConfig();
+        this.config.verticalPosition = 'top';
+        this.config.duration = 5000;
     }
 
     ngOnDestroy(): void {
+        if (!isUndefined(this.deleteAccountSubscription)) {
+            this.deleteAccountSubscription.unsubscribe();
+        }
     }
 
     onDeleteAll() {
-        this.userService.destroyEntireUser().subscribe((success) => {
-            if (success) {
-                this.dialogRef.close();
-                this.loginService.logout();
-                this.router.navigate(['/']);
+        this.principal.identity().then((account) => {
+            if (account.login === this.username) {
+                this.deleteAccountSubscription = this.userService.destroyEntireUser().subscribe((success) => {
+                    if (success) {
+                        this.dialogRef.close();
+                        this.loginService.logout();
+                        this.router.navigate(['/']);
+                    } else {
+                        const message = 'An error occured when deleting your account, please retry !!';
+                        this.config.data = {icon: 'fa-exclamation-circle', text: message}
+                        this.snackBar.openFromComponent(SnackComponent, this.config);
+                    }
+                });
             } else {
-                const config = new MatSnackBarConfig();
-                config.verticalPosition = 'top';
-                config.duration = 20000;
-
-                const message = 'An error occured when deleting your account, please retry !!';
-                config.data = {icon: 'fa-check-circle-o', text: message}
-                this.snackBar.openFromComponent(SnackComponent, config);
+                const message = 'Not the right username';
+                this.config.data = {icon: 'fa-exclamation-circle', text: message}
+                this.snackBar.openFromComponent(SnackComponent, this.config);
             }
         });
+
     }
 
 }
