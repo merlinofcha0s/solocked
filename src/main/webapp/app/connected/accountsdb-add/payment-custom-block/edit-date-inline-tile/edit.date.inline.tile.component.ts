4898798bae1a750253a791df16347d0692614725
raiden <casaucau.cyril@protonmail.com>
2017-11-27 13:34:19 +0100

    Fix datepicker problem when clickoutside

--- src/main/webapp/app/connected/accountsdb-add/payment-custom-block/edit-date-inline-tile/edit.date.inline.tile.component.ts
+++ src/main/webapp/app/connected/accountsdb-add/payment-custom-block/edit-date-inline-tile/edit.date.inline.tile.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
-import {MatDatepickerInputEvent} from '@angular/material';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
+import {MatDatepicker, MatDatepickerInputEvent} from '@angular/material';
+import {isUndefined} from "util";
 
 @Component({
     selector: 'jhi-edit-date-inline',
@@ -10,6 +11,8 @@ export class EditDateInlineTileComponent implements OnInit, OnDestroy {
 
     over: boolean;
     edit: boolean;
+
+    @ViewChild(MatDatepicker) picker;
 
     @Input() placeholder: Date;
     @Input() value: Date;
@@ -29,7 +32,7 @@ export class EditDateInlineTileComponent implements OnInit, OnDestroy {
     }
 
     onClickOutSide(event: any) {
-        if (event && event['value'] === true) {
+        if (event && event['value'] === true && (isUndefined(this.picker) || !this.picker.opened)) {
             this.edit = false;
             this.over = false;
         }
