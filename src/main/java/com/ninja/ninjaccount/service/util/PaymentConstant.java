ba56708bf24368919b71bd4780d49f58baac9888
raiden <casaucau.cyril@protonmail.com>
2017-12-22 17:31:54 +0100

    Raise limit number accounts in beta

--- src/main/java/com/ninja/ninjaccount/service/util/PaymentConstant.java
+++ src/main/java/com/ninja/ninjaccount/service/util/PaymentConstant.java
@@ -3,7 +3,7 @@ package com.ninja.ninjaccount.service.util;
 public interface PaymentConstant {
 
     Integer MAX_ACCOUNTS_PREMIUM = 1000;
-    Integer MAX_ACCOUNTS_BETA = 10;
+    Integer MAX_ACCOUNTS_BETA = 1000;
     Integer MAX_ACCOUNTS_FREE = 10;
 
 }
