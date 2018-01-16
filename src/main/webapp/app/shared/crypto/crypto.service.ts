d3c0d4594652cd2ec2530f757aa501849495c495
raiden <casaucau.cyril@protonmail.com>
2018-01-16 17:44:53 +0100

    Simplifying sum generation from client

--- src/main/webapp/app/shared/crypto/crypto.service.ts
+++ src/main/webapp/app/shared/crypto/crypto.service.ts
@@ -135,22 +135,9 @@ export class CryptoService {
         const msgBuffer = new TextEncoder('utf-8').encode(accountDBB64);
         // hash the message
         const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-        // convert bytes to hex string
-        var hexCodes = [];
-        var view = new DataView(hashBuffer);
-        for (var i = 0; i < view.byteLength; i += 4) {
-            // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
-            var value = view.getUint32(i)
-            // toString(16) will give the hex representation of the number without padding
-            var stringValue = value.toString(16)
-            // We use concatenation and slice for padding
-            var padding = '00000000'
-            var paddedValue = (padding + stringValue).slice(-padding.length)
-            hexCodes.push(paddedValue);
-        }
-
-        console.log('hash : ' + hexCodes.join(""));
+        // convert ArrayBuffer to Array
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
         // Join all the hex strings into one
-        return hexCodes.join("");
+        return hashArray.map((b) => ('00' + b.toString(16)).slice(-2)).join('');
     }
 }
