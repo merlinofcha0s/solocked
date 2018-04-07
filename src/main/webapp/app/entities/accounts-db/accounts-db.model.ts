6be817a7cf410916485fc79fe665257e7be53b5f
raiden <cyril.casaucau@supinfo.com>
2018-04-07 12:17:05 +0200

    Generated with JHipster 4.14.2

--- src/main/webapp/app/entities/accounts-db/accounts-db.model.ts
+++ src/main/webapp/app/entities/accounts-db/accounts-db.model.ts
@@ -8,6 +8,7 @@ export class AccountsDB implements BaseEntity {
         public database?: any,
         public nbAccounts?: number,
         public sum?: string,
+        public userLogin?: string,
         public userId?: number,
     ) {
     }
--- src/main/webapp/app/entities/payment/payment.model.ts
+++ src/main/webapp/app/entities/payment/payment.model.ts
@@ -12,6 +12,7 @@ export class Payment implements BaseEntity {
         public price?: number,
         public planType?: PlanType,
         public paid?: boolean,
+        public userLogin?: string,
         public userId?: number,
     ) {
         this.paid = false;
