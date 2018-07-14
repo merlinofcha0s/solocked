9be76f6d23fa83890ab08702055e90902cc3650a
raiden <casaucau.cyril@protonmail.com>
2018-07-14 17:33:05 +0200

    Fix e2e test

--- src/test/javascript/protractor.conf.js
+++ src/test/javascript/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-            args: ["--headless", "--disable-gpu", "--window-size=1280,800"]
+            args: ["--disable-gpu", "--window-size=1280,800"]
         }
     },
 
