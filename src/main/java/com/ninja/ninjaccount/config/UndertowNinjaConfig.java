2f1df772f0458151b75ea8e4fa73ad98a478dd3f
raiden <casaucau.cyril@protonmail.com>
2018-07-14 23:07:24 +0200

    Modifying undertow configuration

--- src/main/java/com/ninja/ninjaccount/config/UndertowNinjaConfig.java
+++ src/main/java/com/ninja/ninjaccount/config/UndertowNinjaConfig.java
@@ -1,6 +1,7 @@
 package com.ninja.ninjaccount.config;
 
 import io.github.jhipster.config.JHipsterProperties;
+import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.embedded.undertow.UndertowBuilderCustomizer;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -15,19 +17,17 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty("server.ssl.ciphers")
 public class UndertowNinjaConfig {
 
-    private final UndertowServletWebServerFactory factory;
     private final Logger log = LoggerFactory.getLogger(UndertowNinjaConfig.class);
 
-    public UndertowNinjaConfig(UndertowServletWebServerFactory undertowServletWebServerFactory) {
-        this.factory = undertowServletWebServerFactory;
-        this.listenToPortHTTP();
-    }
-
-    private void listenToPortHTTP() {
-        this.log.info("Opening http port");
-        this.factory.addBuilderCustomizers((builder) -> {
+    @Bean
+    public UndertowServletWebServerFactory servletWebServerFactory() {
+        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
+        factory.addBuilderCustomizers((UndertowBuilderCustomizer) builder -> {
+            log.info("listening to http port");
             builder.addHttpListener(80, "0.0.0.0");
+            log.info("Forcing cipher order");
             builder.setSocketOption(UndertowOptions.SSL_USER_CIPHER_SUITES_ORDER, Boolean.TRUE);
         });
+        return factory;
     }
 }
