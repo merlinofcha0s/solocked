beb5672ece115d3067a201b13750379d75fdb829
raiden <casaucau.cyril@protonmail.com>
2018-07-23 09:00:06 +0200

    Desactivation obfuscation

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -125,26 +125,6 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             minimize: true,
             debug: false
         }),
-        new JavaScriptObfuscator ({
-            compact: true,
-            controlFlowFlattening: true,
-            controlFlowFlatteningThreshold: 0.75,
-            deadCodeInjection: true,
-            deadCodeInjectionThreshold: 0.4,
-            debugProtection: false,
-            debugProtectionInterval: false,
-            disableConsoleOutput: true,
-            identifierNamesGenerator: 'hexadecimal',
-            log: false,
-            renameGlobals: false,
-            rotateStringArray: true,
-            selfDefending: false,
-            stringArray: true,
-            stringArrayEncoding: 'base64',
-            stringArrayThreshold: 0.75,
-            transformObjectKeys: false,
-            unicodeEscapeSequence: false
-        }, ['app/polyfills.**.bundle.js', 'app/global.**.bundle.js', 'app/**.**.chunk.js']),
         /*new WorkboxPlugin.InjectManifest({
             importWorkboxFrom: 'local',
             swSrc: 'src/main/webapp/src-service-worker.js',
