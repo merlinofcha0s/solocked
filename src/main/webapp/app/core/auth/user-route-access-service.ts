a5e54de9e21a96efecc1c203da09839a6c22fce4
raiden <casaucau.cyril@protonmail.com>
2018-08-18 15:32:57 +0200

    Correcting problem when accessing access denied page

--- src/main/webapp/app/core/auth/user-route-access-service.ts
+++ src/main/webapp/app/core/auth/user-route-access-service.ts
@@ -47,12 +47,7 @@ export class UserRouteAccessService implements CanActivate {
                 }
 
                 this.stateStorageService.storeUrl(url);
-                this.router.navigate(['accessdenied']).then(() => {
-                    // only show the login dialog, if the user hasn't logged in yet
-                    if (!account) {
-                        this.loginModalService.open();
-                    }
-                });
+                this.router.navigate(['accessdenied']);
                 return false;
             })
         );
